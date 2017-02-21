33b051184472ba6c80a2fc14a83caf628e773e47
Stefan du Fresne <stefan@sdufresne.info>
2017-02-21 08:29:40 +0000

    spalling

--- test/unit/pregnancy_registration.js
+++ test/unit/pregnancy_registration.js
@@ -220,7 +220,7 @@ exports['valid adds lmp_date and patient_id'] = function(test) {
     });
 };
 
-exports['pregnancys on existing patients fail without valid patient id'] = function(test) {
+exports['pregnancies on existing patients fail without valid patient id'] = function(test) {
     sinon.stub(utils, 'getRegistrations').callsArgWith(1, null, []);
     sinon.stub(utils, 'getPatientContactUuid').callsArgWith(2, {statusCode: 404});
 
@@ -243,7 +243,7 @@ exports['pregnancys on existing patients fail without valid patient id'] = funct
     });
 };
 
-exports['pregnancys on existing patients succeeds with a valid patient id'] = function(test) {
+exports['pregnancies on existing patients succeeds with a valid patient id'] = function(test) {
     sinon.stub(utils, 'getRegistrations').callsArgWith(1, null, []);
     sinon.stub(utils, 'getPatientContactUuid').callsArgWith(2, null, {_id: 'uuid'});
 
