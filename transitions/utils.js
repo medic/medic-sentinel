99034c64c43fbc31d1da638bcbddae07f1e59296
Stefan du Fresne <stefan@sdufresne.info>
2017-03-06 12:26:30 +0000

    Generates patient_id on patients medic/medic-webapp#3166

--- transitions/utils.js
+++ transitions/utils.js
@@ -1,4 +1,6 @@
 var _ = require('underscore'),
+    ids = require('../lib/ids'),
+    logger = require('../lib/logger'),
     messages = require('../lib/messages'),
     utils = require('../lib/utils');
 
@@ -26,5 +28,23 @@ module.exports = {
       messages.addMessage(default_msg);
       messages.addError(default_msg.doc, default_msg.message);
     }
+  },
+  addUniqueId: function(db, doc, callback) {
+    var id = ids.generate(doc._id);
+
+    utils.getRegistrations({
+        db: db,
+        id: id
+    }, function(err, registrations) {
+        if (err) {
+            callback(err);
+        } else if (registrations.length) { // id collision, retry
+            logger.warn('Registration ID ' + id + ' is not unique, retrying...');
+            module.exports.addUniqueId(db, doc, callback);
+        } else {
+            doc.patient_id = id;
+            callback();
+        }
+    });
   }
 };
