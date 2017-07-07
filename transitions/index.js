7cac3b46b9a7400d3a5c15ea600f27f45c530184
Milan Andric <milan@medicmobile.org>
2017-07-07 15:54:35 -0300

    created function availableTransitions for export
    another whack at this to make a little more straightforward.

--- transitions/index.js
+++ transitions/index.js
@@ -125,9 +125,7 @@ const loadTransitions = (autoEnableSystemTransitions = true) => {
       return logger.warn(`transition ${transition} is disabled`);
     }
 
-    // Needed to support tests mocking out _loadTransition, and _loadTransition
-    // not being available in prod.
-    (self._loadTransition || self.loadTransition)(transition);
+    self._loadTransition(transition);
   });
 
   // Warn if there are configured transitions that are not available
@@ -381,19 +379,22 @@ const attach = () => {
   });
 };
 
+const availableTransitions = () => {
+  return AVAILABLE_TRANSITIONS;
+};
+
 module.exports = {
+  _loadTransition: loadTransition,
+  _changeQueue: changeQueue,
+  availableTransitions: availableTransitions,
   loadTransitions: loadTransitions,
   canRun: canRun,
   attach: attach,
   finalize: finalize,
   applyTransition: applyTransition,
-  applyTransitions: applyTransitions
+  applyTransitions: applyTransitions,
 };
 
-if (process.env.TEST_ENV) {
-  module.exports._loadTransition = loadTransition;
-  module.exports._changeQueue = changeQueue;
-  module.exports._AVAILABLE_TRANSITIONS = AVAILABLE_TRANSITIONS;
-} else {
+if (!process.env.TEST_ENV) {
   loadTransitions();
 }
