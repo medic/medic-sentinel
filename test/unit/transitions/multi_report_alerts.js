c1f62503d375d06cf38921eda02f5b9b5f5702f6
Stefan du Fresne <stefan@sdufresne.info>
2017-09-14 16:10:22 +0100

    Just use promises
    This batching code wrapped a promise in a callback in a promise.
    
    Instead, just use promises.

--- test/unit/transitions/multi_report_alerts.js
+++ test/unit/transitions/multi_report_alerts.js
@@ -522,3 +522,22 @@ exports['latest report has to go through is_report_counted function'] = test => 
     test.done();
   });
 };
+
+exports['getCountedReportsAndPhones batches properly'] = test => {
+  const report = () => ({ _id: 'docA', form: 'A', contact: { _id: 'contactA' } });
+
+  const firstBatch = [...Array(100).keys()].map(report);
+  const secondBatch = [...Array(50).keys()].map(report);
+
+  const stub = sinon.stub(utils, 'getReportsWithinTimeWindow');
+  stub.onCall(0).returns(Promise.resolve(firstBatch));
+  stub.onCall(1).returns(Promise.resolve(secondBatch));
+
+  transition._getCountedReportsAndPhones(alert, doc)
+    .then(({countedReportsIds, newReports}) => {
+      // Two batches above, plus the doc passed in
+      test.equal(countedReportsIds.length, 151);
+      test.equal(newReports.length, 151);
+      test.done();
+    });
+};
