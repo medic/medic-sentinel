41b166c60f1ef3fd2490fc917b56b58094f09443
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2017-10-30 12:20:30 +1300

    Rework to use couchdb views instead of lucene

--- db.js
+++ db.js
@@ -1,7 +1,5 @@
 var nano = require('nano'),
-	url = require('url'),
-    path = require('path'),
-    request = require('request');
+	url = require('url');
 
 var couchUrl = process.env.COUCH_URL;
 if (couchUrl) {
@@ -9,7 +7,6 @@ if (couchUrl) {
     couchUrl = couchUrl.replace(/\/$/, '');
     var parsedUrl = url.parse(couchUrl);
     var baseUrl = couchUrl.substring(0, couchUrl.indexOf('/', 10));
-    var luceneUrl = baseUrl.replace('5984', '5985');
 
     module.exports = nano(baseUrl);
     module.exports.medic = nano(couchUrl);
@@ -29,46 +26,10 @@ if (couchUrl) {
         module.exports.settings.username = parsedUrl.auth.substring(0, index);
         module.exports.settings.password = parsedUrl.auth.substring(index + 1);
     }
-
-    module.exports.fti = function(index, data, cb) {
-        var uri = path.join('local', module.exports.settings.db, '_design',
-                            module.exports.settings.ddoc, index);
-        var url = luceneUrl + '/' + uri;
-
-        if (data.q && !data.limit) {
-            data.limit = 1000;
-        }
-        var opts = { url: url };
-        if (data.q) {
-            opts.method = 'post';
-            opts.form = data;
-        } else {
-            opts.qs = data;
-        }
-
-        request(opts, function(err, response, result) {
-            if (err) {
-                // the request itself failed
-                console.error(err);
-                return cb(new Error('Error when making lucene request'));
-            }
-            try {
-                result = JSON.parse(result);
-            } catch (e) {
-                return cb(e);
-            }
-            if (data.q && !result.rows) {
-                // the query failed for some reason
-                return cb(result);
-            }
-            cb(null, result);
-        });
-    };
 } else if (process.env.TEST_ENV) {
     // Running tests only
     module.exports = {
         use: function() {},
-        fti: function() {},
         medic: {
             view: function() {},
             get: function() {},
